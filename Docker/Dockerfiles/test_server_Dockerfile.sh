900ea6a3b2247ced1b7a952331529861c3795cba
sjplautz <plautzstephen@gmail.com>
2021-03-24 16:16:42 -0400

    reconfiguring services

--- Docker/Dockerfiles/test_server_Dockerfile.sh
+++
@@ -1,7 +0,0 @@
-FROM nginx
-# add a directory to the root for app development
-RUN mkdir /home/myapp && \
-    cd /home/myapp && \  
-    touch dummy.txt && \
-    echo "hey sexy" > dummy.txt
-RUN echo "Hello World... from test server running container"
