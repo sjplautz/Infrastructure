21768244760b457c5f852597d679ffced15ac095
sjplautz <plautzstephen@gmail.com>
2021-03-31 18:02:27 -0400

    fix

---
+++ Scripts/Kubernetes/copy_secret.sh
@@ -0,0 +1,3 @@
+kubectl get secret <secret-name> -n <source-namespace> -o yaml \
+| sed s/"namespace: <source-namespace>"/"namespace: <destination-namespace>"/\
+| kubectl apply -n <destination-namespace> -f -
